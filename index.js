28c761544733fe3b0dc4adc9f88ce569a7009f64
Oksana Mosendz <bagheera03@gmail.com>
2025-10-16 00:22:37 -0500

    add first section, add catApi

--- index.js
+++ index.js
@@ -1,26 +1,48 @@
+const dogIconLink=document.getElementById('dog-icon__link');
+const catIconLink=document.getElementById('cat-icon__link');
+const chooseBlock=document.getElementsByClassName('choose-block')[0];
+const h1=chooseBlock.querySelector('h1')
+const animalsList=document.getElementsByClassName('animals-list')[0]
 
-const apiKey="live_vfFCtnXKbvbxPbEv9tOYIWt5vCODXwDl5vxkHSNcivbBOUXiIbw4dFABes8ogCXX"
-const url="https://api.thedogapi.com/v1/images/search?"
-const limit='limit=10'
+const dogApiKey =
+  "live_vfFCtnXKbvbxPbEv9tOYIWt5vCODXwDl5vxkHSNcivbBOUXiIbw4dFABes8ogCXX";
+const catApiKey= 'live_S33yFEIT1lqQM9WHYwtq7lYtkcpyuFtH6RiJJNgZJhDPtsPro0qtTfF6o8AQAftN'
+const dogUrl = "https://api.thedogapi.com/v1/images/search?";
+const catUrl = "https://api.thecatapi.com/v1/images/search?";
+const limit = "limit=10";
 
-async function dogs() {
-   const response=await fetch(`${url}${limit}&has_breeds=1`,{
-      method: "GET",
-      headers:{
-         'x-api-key': apiKey
-      } 
-   })
-   const data=await response.json();
-   return data;
-} 
-
-
-async function displayDogs(){
-  const dogsList= await dogs();
-  dogsList.forEach((dog)=>{
-  console.log(`This is ${dog.breeds[0].name}. This breed is for ${dog.breeds[0].bred_for} `)
-  })
-
+async function getAnimals(url, apiKey) {
+  const response = await fetch(`${url}${limit}&has_breeds=1`, {
+    method: "GET",
+    headers: {
+      "x-api-key": apiKey,
+    },
+  });
+  const data = await response.json();
+  console.log(data)
+  return data;
 }
 
-displayDogs()
+async function displayAnimals(url, apiKey) {
+  const animalList = await getAnimals(url, apiKey);
+  animalList.forEach((animal) => {
+   const animalItem=document.createElement('li');
+   animalItem.innerHTML=(`<img src='${animal.url}' height='300'>`);
+   animalsList.appendChild(animalItem);
+  });
+}
+
+function hideTitlePage(){
+chooseBlock.style.display="none";
+h1.style.display='block';
+}
+
+dogIconLink.addEventListener('click',(e)=>{
+   e.preventDefault();
+   hideTitlePage();
+   displayAnimals(dogUrl, dogApiKey)});
+
+catIconLink.addEventListener('click',(e)=>{
+   hideTitlePage();
+   e.preventDefault();
+   displayAnimals(catUrl,catApiKey)});
