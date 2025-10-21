d8d6dbd4cd40b460cc52326ea887b116e2be4cd6
Oksana Mosendz <bagheera03@gmail.com>
2025-10-21 18:04:18 -0500

    add breed fetch,buttons,footer,nav, styles

--- index.js
+++ index.js
@@ -1,48 +1,146 @@
-const dogIconLink=document.getElementById('dog-icon__link');
-const catIconLink=document.getElementById('cat-icon__link');
-const chooseBlock=document.getElementsByClassName('choose-block')[0];
-const h1=chooseBlock.querySelector('h1')
-const animalsList=document.getElementsByClassName('animals-list')[0]
+// start section
+const startSection = document.querySelector(".start-section");
+const dogIconLink = document.getElementById("dog-icon__link");
+const catIconLink = document.getElementById("cat-icon__link");
+const h1 = document.querySelector("h1");
 
-const dogApiKey =
-  "live_vfFCtnXKbvbxPbEv9tOYIWt5vCODXwDl5vxkHSNcivbBOUXiIbw4dFABes8ogCXX";
-const catApiKey= 'live_S33yFEIT1lqQM9WHYwtq7lYtkcpyuFtH6RiJJNgZJhDPtsPro0qtTfF6o8AQAftN'
-const dogUrl = "https://api.thedogapi.com/v1/images/search?";
-const catUrl = "https://api.thecatapi.com/v1/images/search?";
-const limit = "limit=10";
+// animal section
+const animalsSection = document.querySelector(".animals-section");
+const animalsList = document.querySelector(".animals-list");
+const currentAnimal = document.querySelector(".current-animal");
+const loadButton = document.querySelector("button");
 
-async function getAnimals(url, apiKey) {
-  const response = await fetch(`${url}${limit}&has_breeds=1`, {
-    method: "GET",
-    headers: {
-      "x-api-key": apiKey,
-    },
-  });
-  const data = await response.json();
-  console.log(data)
-  return data;
+// nav
+const catsNavLink = document.querySelector(".cats-link");
+const dogsNavLink = document.querySelector(".dogs-link");
+
+// api data
+const dogs = {
+  apiKey:
+    "live_vfFCtnXKbvbxPbEv9tOYIWt5vCODXwDl5vxkHSNcivbBOUXiIbw4dFABes8ogCXX",
+  url: "https://api.thedogapi.com/v1",
+};
+
+const cats = {
+  apiKey:
+    "live_S33yFEIT1lqQM9WHYwtq7lYtkcpyuFtH6RiJJNgZJhDPtsPro0qtTfF6o8AQAftN",
+  url: "https://api.thecatapi.com/v1",
+};
+
+const limit = "limit=6";
+
+let currentOpenAnimal = {
+  apiKey: "",
+  url: "",
+};
+
+async function getAnimals(animals) {
+  try {
+    const response = await fetch(
+      `${animals.url}/images/search?${limit}&has_breeds=1$size=small`,
+      {
+        method: "GET",
+        headers: {
+          "x-api-key": animals.apiKey,
+        },
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error("Error with getting  animals data");
+    }
+
+    const data = await response.json();
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-async function displayAnimals(url, apiKey) {
-  const animalList = await getAnimals(url, apiKey);
+async function getBreedInfo(animals, breedId) {
+  try {
+    const response = await fetch(`${animals.url}/breeds/${breedId}`, {
+      method: "GET",
+      headers: {
+        "x-api-key": animals.apiKey,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error("Error with getting breed data");
+    }
+
+    const data = await response.json();
+
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function diasplayBreedInfo(animals, breedId, img) {
+  const breed = await getBreedInfo(animals, breedId);
+  currentAnimal.style.display = "flex";
+
+  currentAnimal.innerHTML = `<div class="animal-info">
+
+     <img alt=${breed.name} loading="lazy" class='current-img' src=${img}> 
+      <div class="breed-info">
+      <h2 class="breed-name">${breed.name}</h2>
+      <p><span>Life:</span> ${breed.life_span}</p>
+      <p> <span>Temperament:</span>${breed.temperament}</p>
+      </div>`;
+
+  document.body.style.overflow = "hidden";
+  currentAnimal.addEventListener("click", () => {
+    currentAnimal.innerHTML = "";
+    currentAnimal.style.display = "none";
+    document.body.style.overflow = "";
+  });
+}
+
+async function displayAnimalsList(animals) {
+  const animalList = await getAnimals(animals);
+  currentOpenAnimal.url = animals.url;
+  currentOpenAnimal.apiKey = animals.apiKey;
+
   animalList.forEach((animal) => {
-   const animalItem=document.createElement('li');
-   animalItem.innerHTML=(`<img src='${animal.url}' height='300'>`);
-   animalsList.appendChild(animalItem);
+    const animalItem = document.createElement("li");
+
+    animalItem.className = "animal-item";
+    animalItem.innerHTML = `<div class="animal-img__wrap"><img alt='${animal.breeds[0].name}' src='${animal.url}'></div>`;
+    animalsList.appendChild(animalItem);
+    animalsSection.style.display = "block";
+
+    animalItem.addEventListener("click", (e) => {
+      diasplayBreedInfo(animals, animal.breeds[0].id, animal.url);
+    });
   });
 }
 
-function hideTitlePage(){
-chooseBlock.style.display="none";
-h1.style.display='block';
+function hideStartSection() {
+  startSection.style.display = "none";
+  h1.style.display = "block";
 }
 
-dogIconLink.addEventListener('click',(e)=>{
-   e.preventDefault();
-   hideTitlePage();
-   displayAnimals(dogUrl, dogApiKey)});
+// Eventlisteners
 
-catIconLink.addEventListener('click',(e)=>{
-   hideTitlePage();
-   e.preventDefault();
-   displayAnimals(catUrl,catApiKey)});
+function addClickEvent(el, animals) {
+  el.addEventListener("click", (e) => {
+    animalsList.innerHTML = "";
+    e.preventDefault();
+    hideStartSection();
+    displayAnimalsList(animals);
+  });
+}
+
+addClickEvent(catIconLink, cats);
+addClickEvent(catsNavLink, cats);
+addClickEvent(dogIconLink, dogs);
+addClickEvent(dogsNavLink, dogs);
+
+loadButton.addEventListener("click", () => {
+  displayAnimalsList(currentOpenAnima);
+});
